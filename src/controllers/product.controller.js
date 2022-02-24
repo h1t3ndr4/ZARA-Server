da038740ada6cade07af908132a7528e3acde012
Anjum Ara <anjumara0711@gmail.com>
2022-02-24 17:39:20 +0530

    filter

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -21,7 +21,51 @@ router.get("", async (req, res) => {
       return res.send(result);
     }
 
+    if(req.query.price){
+      const result1 = products.filter((product) => {
+        return product.price <= req.query.price;
+        
+      });
+      return res.send(result1);
+    }
+
+    if(req.query.price1){
+      const result2 = products.filter((product) => {
+        return product.price >= req.query.price1;
+        
+      });
+      return res.send(result2);
+    }
+
     return res.send(products);
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+router.get("/:highToLow", async (req, res) => {
+  try {
+    const products = await Product.find().lean().exec();
+
+    const high = products.sort(function(a,b){
+      return b.price - a.price
+    })
+    return res.send(high);
+
+  } catch (e) {
+    res.status(500).send(e.message);
+  }
+});
+
+router.get("/:lowToHigh", async (req, res) => {
+  try {
+    const products = await Product.find().lean().exec();
+
+    const low = products.sort(function(a,b){
+      return a.price - b.price
+    })
+    return res.send(low);
+
   } catch (e) {
     res.status(500).send(e.message);
   }
